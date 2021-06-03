cfbfd17e4e7c0d0fffcf2d3cbabc22462af92947
Crowdbotics <team@crowdbotics.com>
2021-06-03 12:27:12 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -8,26 +8,7 @@ class CustomText(models.Model):
         max_length=150,
     )
 
-    def __str__(self):
-        return self.title
-
-    @property
-    def api(self):
-        return f"/api/v1/customtext/{self.id}/"
-
-    @property
-    def field(self):
-        return "title"
-
 
 class HomePage(models.Model):
     "Generated Model"
     body = models.TextField()
-
-    @property
-    def api(self):
-        return f"/api/v1/homepage/{self.id}/"
-
-    @property
-    def field(self):
-        return "body"
